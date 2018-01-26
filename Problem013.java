986fe4f5ddf034e0a8ec1eb6602369ba07bc0247
Alexdpatel <alexdpatel@gmail.com>
2018-01-25 18:15:43 -0600

    cleaned up and deleted unneeded array

--- Problem013.java
+++ Problem013.java
@@ -14,9 +14,14 @@ public class Problem013 {
         int length = s.length() - 1;
 
         HashMap<String, Integer> indexes = new HashMap<String, Integer>();
-        intializeHashMap(indexes);
+        indexes.put("M", 1000);
+        indexes.put("D", 500);
+        indexes.put("C", 100);
+        indexes.put("L", 50);
+        indexes.put("X", 10);
+        indexes.put("V", 5);
+        indexes.put("I", 1);
 
-        int[] vals = new int[] {1000, 500, 100, 50, 10, 5, 1};
         int sum = 0;
 
         for(int i = 0; i < length; i++)
@@ -24,25 +29,14 @@ public class Problem013 {
             int currentIndex = indexes.get(s.substring(i, i + 1));
             int nextIndex = indexes.get(s.substring(i + 1, i + 2 ));
 
-            if(currentIndex > nextIndex)
-                sum -= vals[currentIndex];
+            if(currentIndex < nextIndex)
+                sum -= currentIndex;
             else
-                sum += vals[currentIndex];
+                sum += currentIndex;
         }
 
-        sum += vals[indexes.get(s.substring(length))];
+        sum += indexes.get(s.substring(length));
 
         return sum;
-    }
-
-    public static void intializeHashMap(HashMap<String, Integer> map)
-    {
-        map.put("M", 0);
-        map.put("D", 1);
-        map.put("C", 2);
-        map.put("L", 3);
-        map.put("X", 4);
-        map.put("V", 5);
-        map.put("I", 6);
     }
 }
